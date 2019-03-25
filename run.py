9772e4a1195af83504b3e3d7aae2390645b753da
Завьялов Андрей <aozavyalov@protonmail.com>
2019-03-25 22:28:31 +0300

    Updated run for new struct

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ import logging
 import sys
 from argparse import ArgumentParser, Namespace
 
-import bot
-from config import TOKENS
+from bot import run
+from bd import init_db
+from config import TOKENS, BD_CREDENTIALS
 
 LOGGING_LEVELS = {
     'TEST': logging.DEBUG,
@@ -13,7 +14,7 @@ LOGGING_LEVELS = {
 def parse_argv() -> Namespace:
     parser = ArgumentParser(description="Starts telegram bot for lessons materials")
     parser.add_argument('action', type=str,
-                        help="run|update_schedules|init_db")
+                        help="run|init_db")
     parser.add_argument('--token', '-t', type=str, default="TEST",
                         help="api token name (from config) for access to bot")
     parser.add_argument('--workers', '-w', type=int, default=10,
@@ -24,12 +25,12 @@ def parse_argv() -> Namespace:
 if __name__ == "__main__":
     args = parse_argv()
     if args.action == "init_db":
-        bot.init_db()
+        init_db(BD_CREDENTIALS)
     elif args.action == "run":
-        bot.run(
+        run(
             token=TOKENS.get(args.token.upper(), TOKENS["TEST"]),
             logger_level=LOGGING_LEVELS.get(args.token.upper(), logging.DEBUG),
             workers=args.workers
         )
     else:
-print(f"Wrong command: {args.action}", file=sys.stderr)
+        print(f"Wrong command: {args.action}", file=sys.stderr)
